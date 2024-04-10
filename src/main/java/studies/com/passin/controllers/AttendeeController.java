4bcdb62a42475a888d15cfed23d2d93803ec94f3
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-04-10 08:21:39 -0300

    feat: Creating endpoint to return attende's badge

--- src/main/java/studies/com/passin/controllers/AttendeeController.java
+++ src/main/java/studies/com/passin/controllers/AttendeeController.java
@@ -1,17 +1,26 @@
 package studies.com.passin.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import studies.com.passin.dto.attendee.AttendeeBadgeResponseDTO;
+import studies.com.passin.services.AttendeeService;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
 
-    @GetMapping
-    public ResponseEntity<String> getTest(){
-        return ResponseEntity.ok("sucesso!");
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{attendeeId}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder){
+        AttendeeBadgeResponseDTO response =  this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
+        return ResponseEntity.ok(response);
 
     }
 }
