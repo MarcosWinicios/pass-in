3bb58adc86045c006349c5e029906224c8ff21c8
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-04-14 20:30:07 -0300

    doc: Updating database documentation

--- docs/ddl.sql
+++ docs/ddl.sql
@@ -1,29 +1,38 @@
-CREATE TABLE "events" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "details" TEXT,
-    "slug" TEXT NOT NULL,
-    "maximum_attendees" INTEGER
+CREATE TABLE events (
+    id TEXT NOT NULL PRIMARY KEY,
+    title TEXT NOT NULL,
+    details TEXT,
+    slug TEXT NOT NULL,
+    maximum_attendees INTEGER NOT NULL
 );
 
-CREATE TABLE "attendees" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "event_id" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+CREATE TABLE attendees (
+    id TEXT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE "check_ins" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "attendeeId" TEXT NOT NULL,
-    CONSTRAINT "check_ins_attendeeId_fkey" FOREIGN KEY ("attendeeId") REFERENCES "attendees" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+CREATE TABLE attendees_events (
+    id SERIAL NOT NULL PRIMARY KEY,
+    id_event TEXT NOT NULL,
+    id_attendee TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_event) REFERENCES events(id)
+		ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (id_attendee) REFERENCES attendees(id)
+		ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE UNIQUE INDEX "events_slug_key" ON "events"("slug");
+CREATE TABLE check_ins (
+    id SERIAL NOT NULL PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    attendees_events_id INTEGER NOT NULL,
+    FOREIGN KEY (attendees_events_id) REFERENCES attendees_events (id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+);
 
-CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
+CREATE UNIQUE INDEX events_slug_key ON events(slug);
 
-CREATE UNIQUE INDEX "check_ins_attendeeId_key" ON "check_ins"("attendeeId");
+CREATE UNIQUE INDEX check_ins_attendee_id_key ON check_ins(attendees_events_id);
